c5ad35fa617209263960b57197f5da493cac44c1
Eduardo <eduardovcn74@gmail.com>
2025-08-29 21:57:50 -0300

    inserting Saque and Deposito Classes

--- des_sistema_bancario.py
+++ des_sistema_bancario.py
@@ -1,5 +1,5 @@
 
-from abc import ABC, abstractmethod
+from abc import ABC
 from datetime import datetime
 
 LIMITE_SAQUES = 3
@@ -150,11 +150,30 @@ class Transacao(ABC):
     def registrar(self, conta):
         pass
 
+class Saque(Transacao):
+    def __init__(self, valor):
+        self._valor = valor
 
+    @property
+    def valor(self):
+        return self._valor
 
+    def registrar(self, conta):
+        sucesso_transacao = conta.sacar(self.valor)
+        
+        if sucesso_transacao:
+            conta.historico.adicionar_transacao(self)
 
+class Deposito(Transacao):
+    def __init__(self, valor):
+        self._valor = valor
 
+    @property
+    def valor(self):
+        return self._valor
 
-
-
-
+    def registrar(self, conta):
+        sucesso_transacao = conta.depositar(self.valor)
+        
+        if sucesso_transacao:
+            conta.historico.adicionar_transacao(self)
